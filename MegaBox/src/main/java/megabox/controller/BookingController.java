caa115fa2defc304d27acc57b3f8f4ebbd506254
seojw0730 <seojw0730@naver.com>
2024-03-20 09:20:47 +0900

    doPost 삭제

--- MegaBox/src/main/java/megabox/controller/BookingController.java
+++ MegaBox/src/main/java/megabox/controller/BookingController.java
@@ -30,12 +30,4 @@ public class BookingController extends HttpServlet {
 		request.getRequestDispatcher("WEB-INF/views/booking.jsp").forward(request, response);
 	}
 
-//	/**
-//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		doGet(request, response);
-//	}
-
 }

003d11f8dfd83e2b2b2ab88fb74471f312a8a041
HG3732 <gyrua34@gmail.com>
2024-03-20 14:41:32 +0900

    Update MainController.java

--- MegaBox/src/main/java/megabox/controller/MainController.java
+++ MegaBox/src/main/java/megabox/controller/MainController.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/maincontroller")
+@WebServlet("/main")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/header.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/main/main.jsp").forward(request, response);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

72413de2794ab4eefb0e42abe040214e0e93aa82
HG3732 <gyrua34@gmail.com>
2024-03-21 20:52:33 +0900

    Update TheatreController.java

--- MegaBox/src/main/java/megabox/controller/TheatreController.java
+++ MegaBox/src/main/java/megabox/controller/TheatreController.java
@@ -7,27 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TheatreController
- */
 @WebServlet("/theatre")
 public class TheatreController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TheatreController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/views/main/theater/theatre.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/main/theater/theater.jsp").forward(request, response);
 	}
 
 }

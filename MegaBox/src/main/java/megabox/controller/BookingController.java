5a04ba23d5ef198886901afe5a689ade8c55b3e7
seojw0730 <seojw0730@naver.com>
2024-03-22 17:15:43 +0900

    Update BookingController.java

--- MegaBox/src/main/java/megabox/controller/BookingController.java
+++ MegaBox/src/main/java/megabox/controller/BookingController.java
@@ -27,7 +27,7 @@ public class BookingController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/WEB-INF/views/main/booking/booking/booking.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/main/booking/booking.jsp").forward(request, response);
 	}
 
 }
